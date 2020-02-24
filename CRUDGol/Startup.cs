121f17ec85d058d97843c1d704d77fc9066709ff
DougMike <49522555+DougMike@users.noreply.github.com>
2020-02-23 21:34:49 -0300

    Atualizacao

--- CRUDGol/Startup.cs
+++ CRUDGol/Startup.cs
@@ -29,7 +29,7 @@ namespace CRUDGol
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AeronaveContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
-            services.AddScoped(typeof(BaseRepository<>), typeof(IBaseRepository<>));
+            services.AddScoped<IAeronaveRepository, AeronaveRepository>();
             services.AddControllers();
         }
 
